5a5609c8b3d1f8a786013b366598a55bf0b7f871
sulthonuladib <adibcyber4rt@gmail.com>
2020-10-09 22:43:18 +0700

    Initiate project with start.springboot.io

---
+++ src/main/kotlin/sulthonuladib/kotlin/restful/KotlinRestfulSinauApplication.kt
@@ -0,0 +1,11 @@
+package sulthonuladib.kotlin.restful
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestfulSinauApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestfulSinauApplication>(*args)
+}
---
+++ src/test/kotlin/sulthonuladib/kotlin/restful/KotlinRestfulSinauApplicationTests.kt
@@ -0,0 +1,13 @@
+package sulthonuladib.kotlin.restful
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestfulSinauApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
